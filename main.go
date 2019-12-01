c1a48a3bf636d4e59e221ffc2a8ba4839e9f0126
Arnau Lacambra <naulacambra@gmail.com>
2019-12-01 19:20:37 +0100

    Added ArangoDB connector

--- main.go
+++ main.go
@@ -1,10 +1,41 @@
 package main
 
-import "github.com/gin-gonic/gin"
-import "net/http"
+import (
+	"context"
+	"fmt"
+
+	"github.com/arangodb/go-driver"
+	"github.com/arangodb/go-driver/http"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	conn, err := http.NewConnection(http.ConnectionConfig{
+		Endpoints: []string{"http://localhost:8529"},
+	})
+	if err != nil {
+		fmt.Println("Cannot connect")
+		return
+	}
+	fmt.Println("Could connect")
+	client, err := driver.NewClient(driver.ClientConfig{
+		Connection:     conn,
+		Authentication: driver.BasicAuthentication("root", "root"),
+	})
+	fmt.Println("Could get client")
+	if err != nil {
+		fmt.Println(err)
+	}
+	ctx := context.Background()
+	db, err := client.Database(ctx, "wifi-viewer")
+
+	if err != nil {
+		fmt.Println("Could not get db")
+	}
+	fmt.Println("Could get db")
+
+	found, err := db.CollectionExists(ctx, "myCollection")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("collection is found", found)
 }
-
-
